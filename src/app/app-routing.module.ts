5ebe67489f4d88821b0a4d4b8a2a9af79f05a236
Ram Delatina <ram.delatina.29@gmail.com>
2017-07-12 12:52:39 +0800

    Complete design override & Added TODO list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { IndexComponent } from './components/index/index.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { ForumComponent } from './components/forum/forum.component';
+import { HappeningsComponent } from './components/happenings/happenings.component';
+
 const routes: Routes = [
-  {
-    path: '',
-    children: []
-  }
+  { path: '', component: IndexComponent },
+  { path: 'forum', component: ForumComponent },
+  { path: 'happenings', component: HappeningsComponent },
+  { path: 'users/login', component: LoginComponent },
+  { path: 'users/signup', component: SignupComponent },
+  { path: '**', redirectTo: '', pathMatch: 'full' }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,28 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './components/index/index.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { LoginComponent } from './components/login/login.component';
+import { SignupComponent } from './components/signup/signup.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { ForumComponent } from './components/forum/forum.component';
+import { HappeningsComponent } from './components/happenings/happenings.component';
+import { EventsComponent } from './components/events/events.component';
+import { AnnouncementsComponent } from './components/announcements/announcements.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    NavbarComponent,
+    LoginComponent,
+    SignupComponent,
+    FooterComponent,
+    ForumComponent,
+    HappeningsComponent,
+    EventsComponent,
+    AnnouncementsComponent
   ],
   imports: [
     BrowserModule,
